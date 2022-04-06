326f711da1f88ea6ba049fedd3928a68b99b78a3
Amit Prakash <amit19prakash08@gmail.com>
2022-04-06 21:14:25 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express =require("express");
+const path=require("path")
+const app=express();
+const port=1201;
+
+//Built in middleware
+
+console.log(path.join(__dirname,"/public"));
+const staticpath=path.join(__dirname,"/public");
+app.use(express.static(staticpath));
+
+app.get("/", (req,res)=>{
+    res.send("hello this is Prakash.");
+});
+app.listen(port, () =>{
+    console.log(`listening to the port ${port}`);
+});
---
+++ public/js/main.js
@@ -0,0 +1,85 @@
+//Server Code
+
+/**const http = require('http');
+const fs = require('fs')
+const fileContent = fs.readFileSync('/index.html')
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    //res.statusCode = 200;
+    res.writeHead(200, {'Content-type':'text/html'})
+    //res.setHeader('Content-Type', 'text/html');
+    res.end(fileContent);
+});
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);*/
+
+(function ($) {
+    "use strict";
+
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+    // Date and time picker
+    $('#date').datetimepicker({
+        format: 'L'
+    });
+    $('#time').datetimepicker({
+        format: 'LT'
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        center: true,
+        autoplay: true,
+        smartSpeed: 2000,
+        dots: true,
+        loop: true,
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+    
+})(jQuery);
+
+//});
