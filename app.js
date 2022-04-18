f9ef7fc5390f5a5ecf1802ff1b60339e87e395cf
Amit Prakash <amit19prakash08@gmail.com>
2022-04-19 00:05:43 +0530

    nodemon app Workflows modified-7

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express =require("express");
 const path=require("path")
 const app=express();
 const port= process.env.PORT || 5000;
-const config = require('config')
 
 //Built in middleware
 
