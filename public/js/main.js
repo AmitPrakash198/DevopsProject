6d8ed84b41c0d7ce2b13a21852f23f3938b3502f
Amit Prakash <amit19prakash08@gmail.com>
2022-04-28 21:42:17 +0530

    Title color change

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,5 @@
 //Server Code
 
-/**const http = require('http');
-const fs = require('fs')
-const fileContent = fs.readFileSync('/index.html')
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    //res.statusCode = 200;
-    res.writeHead(200, {'Content-type':'text/html'})
-    //res.setHeader('Content-Type', 'text/html');
-    res.end(fileContent);
-});
-server.listen(port, hostname, () => {
-console.log(`Server running at http://${hostname}:${port}/`);*/
-
 (function ($) {
     "use strict";
 
@@ -81,5 +66,3 @@ console.log(`Server running at http://${hostname}:${port}/`);*/
     });
     
 })(jQuery);
-
-//});
